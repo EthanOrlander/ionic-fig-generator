2ded0f1718803700f3da01e70a58138e9f08272e
Ethan Orlander <ethanorlander@gmail.com>
2021-08-14 15:14:31 -0400

    tidy up

--- README.md
+++ README.md
@@ -1,8 +1,6 @@
-Generate fig specs for ionic cli.
-Ionic cli defined in commands.json.
-commands.json generated with `ionic help --json > commands.json`
+# Generate fig specs for ionic cli
 
-# How to generate spec
+## How to generate spec
 
 1. Clone this repo using `git clone git@github.com:EthanOrlander/ionic-fig-generator.git`
 2. Install dependencies with `yarn`
---
+++ index.ts
@@ -0,0 +1,47 @@
+import * as fs from "fs";
+import { exec as OriginalExec } from "child_process";
+import { promisify } from "util";
+
+const exec = promisify(OriginalExec);
+
+const generateCommands = () => exec('yarn ionic help --json');
+
+const attachSubcommand = (completionSpec: Fig.Spec, commandStr: string): Fig.Subcommand => {
+    const commandPieces = commandStr.split(" ").slice(1);
+    let last = completionSpec;
+    if (!last) throw Error('Err');
+    for (const subcommand of commandPieces) {
+        if (!last.subcommands) last.subcommands = [];
+        if (!last.subcommands.find(sc => sc.name === subcommand))
+            last.subcommands.push({ name: subcommand })
+        last = last.subcommands.find(sc => sc.name === subcommand) as Fig.Subcommand;
+    }
+    return last;
+}
+
+const generateSpec = (commands: any): Fig.Spec => {
+    const completionSpec: Fig.Spec = {
+        name: 'ionic',
+        description: 'The Ionic command-line interface (CLI) is the go-to tool for developing Ionic apps.',
+        subcommands: []
+    }
+    for (const command of commands.commands) {
+        const subcommand: Fig.Subcommand = attachSubcommand(completionSpec, command.name);
+        subcommand.options = command.options.map((option: any) => {
+            const args: Fig.Arg | undefined = option.type === 'string' ? { name: option.spec.value } : undefined;
+            const names = [`--${option.name}`].concat(option.aliases.map((alias: string) => `-${alias}`));
+            return ({ name: names, description: option.summary, args: args })
+        })
+        subcommand.args = command.inputs.map((input: any) => ({ name: input.name, description: input.summary, isOptional: !input.required }));
+    }
+    fs.writeFileSync('out/spec.json', JSON.stringify(completionSpec));
+    return completionSpec;
+}
+
+(async () => {
+    const { stdout } = await generateCommands();
+    const sanitizedOutput = stdout.split("\n")[1];
+    const commands = JSON.parse(sanitizedOutput);
+    generateSpec(commands);
+    console.log("Fig autocomplete spec generated successfully!");
+})();
